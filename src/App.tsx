64360d55b959c24fa5aab5dff8d295bb1eb4aa7d
Leonardo Perez <leonardopl617@gmail.com>
2026-05-13 09:58:22 -0600

    feat: Counter y button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 // src/App.tsx
 import Saludo from './Saludo';
-
+import { useState } from 'react';
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div>
+      <p>Contador: {counter}</p>
+      <button onClick={() => setCounter(counter + 1)}>
+        Incrementar
+      </button>
       {/* Enviamos los datos específicos como si fueran atributos HTML */}
       <Saludo nombre="Leonardo" momento="buenas tardes" />
     </div>
--- src/Saludo.tsx
+++ src/Saludo.tsx
@@ -1,12 +1,8 @@
-// src/Saludo.tsx
-
-// 1. Definimos la estructura de las props con TypeScript
 interface SaludoProps {
     nombre: string;
     momento: string;
 }
 
-// 2. Extraemos las props directamente en los parámetros de la función
 export default function Saludo({ nombre, momento }: SaludoProps) {
     return (
         <div>
